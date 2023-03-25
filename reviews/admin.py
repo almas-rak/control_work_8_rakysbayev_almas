6c808388b23c69924d1bfb942a6a910ee8a55d76
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-03-25 13:03:46 +0600

    регистрация продукта в админке

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from reviews.models import Product
+
+
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'product_category', 'product_image')
+    list_editable = ('product_name', 'product_category', 'product_image')
+
+
+admin.site.register(Product, ProductAdmin)
